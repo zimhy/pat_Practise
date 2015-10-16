cb262bf3ea4ae622a3af32b9c1221181569c4db9
menghaizhang <menghaizhang@menghaizhangdemacbook-air.local>
2015-10-16 11:18:58 +0800

    pat的练习题

---
+++ pat_Practise/pat_Practise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pat_Practise
+//
+//  Created by menghaizhang on 15/10/16.
+//  Copyright © 2015年 menghaizhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
