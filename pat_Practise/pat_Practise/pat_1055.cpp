6d25f045e46eb82aa2459935e850d27c548ac744
zimhy <466916676@qq.com>
2015-10-23 21:26:41 +0800

    1055 一个超时

--- pat_Practise/pat_Practise/pat_1055.cpp
+++ pat_Practise/pat_Practise/pat_1055.cpp
@@ -16,7 +16,7 @@ struct Person {
 	int age;
 	int wealth;
 };
-bool compare(Person &a, Person &b)
+bool compare(const Person &a,const  Person &b)
 {
 	if(a.wealth != b.wealth)
 		return a.wealth > b.wealth;
@@ -48,56 +48,68 @@ int main (int argc, const char * argv[])
 	vector <Case> cases;
 	int number_per, number_case;
 	cin >> number_per >> number_case;
+	if (number_case > 1000)
+	{
+		number_case = 1000;
+	}
+	
 	for (int i = 0; i < number_per; i++)
 	{
 		string name;
 		int age, wealth;
 		cin >> name >> age >> wealth;
-		Person person = { name ,age ,wealth };
-		persons.push_back(person);
+
+		if (age <= 200)
+		{
+			Person person = { name ,age ,wealth };
+			persons.push_back(person);
+		}
 	}
 
 	sort(persons.begin(),persons.end(), compare);
+	
 	for (int i = 0; i < number_case; i++)
 	{
-		Case out_case  ;
-		cin >> out_case.num >> out_case.min_age >> out_case.max_age;
+		Case out_case;
+		cin >> out_case.num >> out_case.min_age >> out_case.max_age; 
+		if (out_case.num > 100)
+		{
+			out_case.num = 100;
+		}
 		cases.push_back(out_case);
+		
 	}
-	for (int i = 0; i < number_case; i++)
+	
+	for (int i = 0; i < cases.size(); i++)
 	{
 		Case output_case = cases[i];
 		bool is_full = false;
+		bool is_empty = true;
+		int person_count = 0;
+		cout << "Case #" << i + 1 << ":" << endl;
 		for (int j = 0; j<persons.size()&&!is_full; j++)
 		{
+			
 			Person person = persons[j];
 			if (person.age <= output_case.max_age && person.age >= output_case.min_age)
 			{
-				output_case.persons.push_back(person) ;
-				if (output_case.persons.size()>=output_case.num)
-				{
-					is_full = true;
-				}
-			}
-		}
-		cases[i] = output_case;
-	}
-	for (int i = 0; i < cases.size();i++)
-	{
-		Case output_case = cases[i];
-		cout << "Case #" << i + 1 <<":"<< endl;
-		if (output_case.persons.empty())
-			cout << "None" << endl;
-		else
-		{
-			for (int j = 0; j < output_case.persons.size(); j++)
-			{
-				Person person = output_case.persons[j];
+				//output_case.persons.push_back(person) ;
+				is_empty = false;
 				cout << person.name << " " << person.age << " " << person.wealth << endl;
+				person_count++;
+				
+			}
+			if (person_count>= output_case.num)
+			{
+				is_full = true;
 			}
 		}
-
+		if (is_empty)
+		{
+			cout << "None" << endl;
+		}
+		//cases[i] = output_case;
 	}
-
-	cin >> number_case;
+	system("PAUSE");
+	//system.
 }
